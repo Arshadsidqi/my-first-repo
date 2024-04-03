d8b517fefdc78a5551a40eb425f9b0b1402d39d8
Arshadsidqi <arshad171999@gmail.com>
2024-04-03 14:11:33 +0530

    add destructuring

---
+++ destructuring.js
@@ -0,0 +1,70 @@
+// let obj = {
+//   name: "arshad",
+//   roll_no: 24,
+//   subject: "javafull stack",
+// };
+//  for object destructuring
+
+// let { name, roll_no, subject } = obj;
+// console.log(name);
+
+
+
+//  for array destructuring
+
+// let arr=[10,20,30,42,56,78,54]
+
+// let [a,b,c,d,e,f,g] = arr;
+//  console.log(a,b,c,d,e,f,g);
+
+//for function  object destructuring
+
+// let obj = {
+//   name: "arshad",
+//   roll_no: 24,
+//   subject: "javafull stack",
+// };
+// function details({ name, roll_no, subject }) {
+//   console.log(name);
+//   console.log(roll_no);
+//   console.log(subject);
+// }
+// details(obj)
+
+
+
+//    function array destructuring
+
+// let obj =[10,52,26,5,25,36,25];
+//   function details([a,b,c,d,e,f,g]) {
+//    console.log(a,b,c,d,e,f,g)
+//   }
+//   details(obj)
+
+
+
+
+//     array destructuring with ...rest 
+
+// let obj =[10,52,26,5,25,36,25];
+//   let [a,...rest]=obj
+//   console.log(a,rest)
+
+
+
+
+//     object destructuring with ...rest 
+
+// let obj = {
+//   name: "arshad",
+//   roll_no: 24,
+//   subject: "javafull stack",
+// };
+ 
+
+// let { name,...rest } = obj;
+// console.log(name,rest);
+
+
+
+
